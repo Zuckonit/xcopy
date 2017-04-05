8eb6c6237d2bd7b50054b7dc8217f95d9d410882
Mocker <lsin30@foxmail.com>
2017-04-05 20:04:16 +0800

    Update xcopy.py

--- xcopy.py
+++ xcopy.py
@@ -1,11 +1,12 @@
 #coding: utf-8
 
 import xerox
+import codecs
 import os.path as osp
 import sys
 
 if __name__ == '__main__':
     filename = sys.argv[1] if len(sys.argv) > 1 else None
     if osp.isfile(filename):
-        with open(filename, 'rb') as f:
+        with codecs.open(filename, 'rb', encoding='utf-8') as f:
             xerox.copy(f.read())
